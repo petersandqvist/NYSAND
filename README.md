5273e261e29bc1079b3bb51d7cf7397ff119a3b3
AnnaLovisa <gribbe.anna@gmail.com>
2017-02-05 19:02:29 +0100

    change

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-Our Website project 'SAND' in HTML, CSS/SASS and a bit of JavaScript. 
+# SAND_FINAL
--- js/main.js
+++ js/main.js
@@ -17,6 +17,7 @@ document.getElementById('hamburgler').addEventListener('click', checkNav);
    }
  }
 
+// addEventListener(closeNav)
 
     function closeNav() {
    document.body.classList.remove('hamburgler-active');
@@ -27,23 +28,7 @@ document.getElementById('hamburgler').addEventListener('click', checkNav);
  }
 
     
-/*SMOOTH SCROLLING*/
 
- $(function() {
-  $('a[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html, body').animate({
-          scrollTop: target.offset().top
-        }, 1000);
-         closeNav();
-        return false;
-      }
-    }
-  });
-});
 
 
 
